9ae4a39527ed2d8f567e650d3728c6e6a8d33fe1
GRAND <bengrand.pro@gmail.com>
2020-04-08 15:00:19 +0200

    changed text

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          HELLO NETIFLY
+          Bienvenue sur Netlify!
         </p>
         
       </header>
